cc9c340d6796183d4a8a86c127d2cb37ae89e1dd
zsh142857 <zhengshunhe142857@gmail.com>
2022-06-10 17:19:54 +0800

    Hello world with class Person

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -2,6 +2,8 @@ package com.tom;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        //System.out.println("Hello world");
+        Person p=new Person();
+        p.greeting();
     }
 }
---
+++ src/main/java/com/tom/Person.java
@@ -0,0 +1,7 @@
+package com.tom;
+
+public class Person {
+    public void greeting() {
+        System.out.println("Hello!");
+    }
+}
